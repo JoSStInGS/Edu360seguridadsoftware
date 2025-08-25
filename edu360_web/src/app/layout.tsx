10fb071e325fa6f46995e6ecf0cbd2437b072588
CodeG-2021 <codeg2016cr@gmail.com>
2025-08-25 15:34:57 -0600

    Login form design added

--- edu360_web/src/app/layout.tsx
+++ edu360_web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
 import "./globals.css";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "Edu360",
